7a4771519fce1d516b262b2aa1a2d50d1077c02b
박찬준 <pcj01142@gmail.com>
2022-03-26 09:06:47 +0900

    text 사용법

--- app/src/main/java/com/jingom/composecanvaspractice/MainActivity.kt
+++ app/src/main/java/com/jingom/composecanvaspractice/MainActivity.kt
@@ -3,13 +3,13 @@ package com.jingom.composecanvaspractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.jingom.composecanvaspractice.practice.BallTrackerGame
+import com.jingom.composecanvaspractice.practice.TextTest
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-			BallTrackerGame()
+			TextTest()
 		}
 	}
 }
---
+++ app/src/main/java/com/jingom/composecanvaspractice/practice/Lesson3.kt
@@ -0,0 +1,29 @@
+package com.jingom.composecanvaspractice.practice
+
+import android.graphics.Color
+import android.graphics.Paint
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.nativeCanvas
+
+@Composable
+fun TextTest() {
+	Canvas(
+		modifier = Modifier.fillMaxSize()
+	) {
+		drawContext.canvas.nativeCanvas.apply {
+			drawText(
+				"This is My Test",
+				100f,
+				200f,
+				Paint().apply {
+					isAntiAlias = true
+					color = Color.RED
+					textSize = 50f
+				}
+			)
+		}
+	}
+}
